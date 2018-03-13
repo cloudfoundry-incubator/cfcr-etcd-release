9ca8183b3b1a12801467f806a0bef15618418911
Morena De Liddo <mdeliddo@pivotal.io>
2018-03-13 12:14:53 +0000

    Extract set-and-check-value code to script
    [#155332654]

---
+++ ci/scripts/set-and-check-value.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -eu
+
+source "$(dirname "$0")/helpers/expose-jumpbox.sh"
+expose_jumpbox "${PWD}" "${JUMPBOX_SSH_KEY}" "${JUMPBOX_URL}"
+bosh ssh etcd/0 -c "ETCDCTL_API=3 /var/vcap/jobs/etcd/bin/etcdctl put test-key test-value"
+bosh ssh etcd/0 -c "ETCDCTL_API=3 /var/vcap/jobs/etcd/bin/etcdctl get test-key | grep test-value"
