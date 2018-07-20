aa3655d8ceeffb5de30f3326142484eb1b783756
Konstantinos Karampogias <konstantinos.karampogias@swisscom.com>
2018-07-20 14:04:06 +0100

    Add shipit job to the pipeline
    [#158946553][#158945642]
    
    Signed-off-by: Akshay Mankar <amankar@pivotal.io>

---
+++ ci/scripts/create-final-release.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -exu -o pipefail
+
+export BOSH_LOG_LEVEL=debug
+export BOSH_LOG_PATH="$PWD/bosh.log"
+version=$(cat cfcr-etcd-version/version)
+git config --global user.name "cfcr"
+git config --global user.email "cfcr@pivotal.io"
+
+cp -r git-cfcr-etcd-release/. git-cfcr-etcd-release-output
+
+cd git-cfcr-etcd-release-output
+
+cat <<EOF > "config/private.yml"
+blobstore:
+  options:
+    access_key_id: ${ACCESS_KEY_ID}
+    secret_access_key: ${SECRET_ACCESS_KEY}
+EOF
+
+bosh create-release --final --version=${version} --sha2 --tarball ../cfcr-etcd-release/cfcr-etcd-release-${version}.tgz
+
+echo "v${version}" >../cfcr-etcd-release/name
+echo "" > ../etcd-release/body
+
+git checkout -b tmp/release
+git add .
+git commit -m "Final release for v${version}"
+git tag -a "v${version}" -m "Tag for version v${version}"
+git checkout "${BRANCH:-master}"
+git merge tmp/release -m "Merge release branch for v${version}"
+git branch -d tmp/release
