e3ee0f68090cbcce46f400737d50ba58c2302f73
Neil Hickey <nhickey@vmware.com>
2021-06-05 20:01:37 -0600

    Revert "Add non-interactive flag"
    This reverts commit e7dc6866a532ac83fd5438ed8ac0ea1f1ac5624f.

--- ci/scripts/delete-vm.sh
+++ ci/scripts/delete-vm.sh
@@ -5,4 +5,4 @@ set -eu
 source "$(dirname "$0")/helpers/expose-jumpbox.sh"
 expose_jumpbox "${PWD}" "${JUMPBOX_SSH_KEY}" "${JUMPBOX_URL}"
 
-bosh --non-interactive -d etcd-multiaz delete-vm etcd/0
+bosh -d etcd-multiaz delete-vm etcd/0
